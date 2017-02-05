847e289db116b2adaab6b5bae1baca6f6d6ba98d
anokata <quartenium@gmail.com>
2017-02-05 10:01:17 +0300

    learn and try various expressions and technics

--- exercises/first.cpp
+++ exercises/first.cpp
@@ -2,35 +2,8 @@
 #include <cmath>
 using namespace std;
 
-int some() {
-    cout << "Hello world!" << endl;
-    some();
-    cout << sizeof(int) << sizeof(long) << sizeof(char) << endl;
-    char c = -2;
-    unsigned char uc = -2;
-    double f = 1.2;
-    cout << (int)c << (int)uc << "  " << f << "  " << int(f) << endl;
-    cout << 'a' << 12311111111L << -23UL << 3.2E-10 << ' ' << (false) << true;
-    string s;
-    int a = 2;
-    int b = 3;
-    b = a+++b;
-    cout << a << b << endl;
-    a = a++ ;
-    cout << a << true == 0;
-    {
-        int a = 1111;
-    }
-    cout << a;
-    do { } while (0);
-    // cin >> a;
-    cout << a;
-    char z;
-    cin.get(z);
-    cin.get(z);
-    cout << z;
-}
 
+#define MAX(x, y, r) {int _a = x; int _b=y; r=_a>_b?_a:_b;}
 void show_array(int array[], unsigned length) {
     cout << '[';
     for (int i = 0; i < length - 1; ++i) {
@@ -108,9 +81,34 @@ void process_one() {
         cout << count;
     }
 }
-// TODO array cmp array
-#define MAX(x, y, r) {int _a = x; int _b=y; r=_a>_b?_a:_b;}
-int main() {
+/*
+int some() {
+    cout << "Hello world!" << endl;
+    some();
+    cout << sizeof(int) << sizeof(long) << sizeof(char) << endl;
+    char c = -2;
+    unsigned char uc = -2;
+    double f = 1.2;
+    cout << (int)c << (int)uc << "  " << f << "  " << int(f) << endl;
+    cout << 'a' << 12311111111L << -23UL << 3.2E-10 << ' ' << (false) << true;
+    string s;
+    int a = 2;
+    int b = 3;
+    b = a+++b;
+    cout << a << b << endl;
+    a = a++ ;
+    cout << a << true == 0;
+    {
+        int a = 1111;
+    }
+    cout << a;
+    do { } while (0);
+    // cin >> a;
+    cout << a;
+    char z;
+    cin.get(z);
+    cin.get(z);
+    cout << z;
     //stack array
     int a[3] = {1, 2, 3};
     show_array(a, 3);
@@ -118,11 +116,23 @@ int main() {
     show_array(a, 3);
     a[0] = true?100:1/0;
     show_array(a, 3);
-    int b = 3; int c = 4; int r = 0;
-    MAX(b+2, c+=4, r);
+    int cc = 4; int r = 0;
+    MAX(b+2, cc+=4, r);
     cout << r << sqrt(2) << endl;
     sq_root_equasion(1, 4, 4);
     cout << '|' << log_int(5) << ' ' << log_int(11539);
-    process_one();
+    //process_one();
+}*/
+// TODO array cmp array
+void one() { int a; cout << a; }
+void tw() { int a = 321; }
+void not_init_on_stack_get_prev_value() {
+    one();
+    tw();
+    one();
+}
+
+
+int main() {
     return 0;
 }
