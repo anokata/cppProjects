77e499de8e17e12d31e2c6b4d9e93db8bcd65d34
anokata <quartenium@gmail.com>
2017-02-07 23:46:08 +0300

    task about shared lib and makefile

--- share_lib/main.c
+++ share_lib/main.c
@@ -1,7 +1,15 @@
 #include "hello.h"
+#include <stdio.h>
+#include <stddef.h>
+#include "solution.h"
 
 int main() {
     hello_message("John");
+    char *s = "Str";
+    int a = 8;
+    int b;
+    b = stringStat(s, 2, &a);
+    printf("\n b = %i a = %i\n", b, a);
     return 0;
 }
 //gcc main.c -L. -lHello -o hello
