b27663a6486771d7b4ac014cccf5d29d68fae552
anokata <quartenium@gmail.com>
2017-02-09 23:55:46 +0300

    load lib and run fun task ok

--- share_lib/main.c
+++ share_lib/main.c
@@ -1,28 +1,22 @@
-//#include "hello.h"
 #include <stdio.h>
 #include <stddef.h>
-//#include "solution.h"
 #include <dlfcn.h>
 
 void (*hello_message)(const char *);
 int (*runtime_fun)(int);
 
-int main() {
-    //hello_message("John");
+int main(int argc, char *argv[]) {
     char *s = "Str";
-    void *lib = dlopen("libHello.so", RTLD_LAZY);
+    void *lib = dlopen("./libHello.so", RTLD_LAZY);
     if (NULL == lib)
         printf("lib not loaded");
     hello_message = (void (*) (const char *))dlsym(lib, "hello_message");
     runtime_fun = (int (*) (int))dlsym(lib, "runtime_fun");
     if (NULL == runtime_fun || NULL == hello_message)
         printf("funcs not founded");
-    hello_message("YEar!");
+    hello_message("11YEar!");
     hello_message(s);
     runtime_fun(123);
-    // cast
-    // void runtime_fun(int a);
-    // run
     return 0;
 }
 //gcc main.c -L. -lHello -o hello
