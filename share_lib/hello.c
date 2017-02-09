294493a78d3481047aeb2970ae62f87a2a233435
anokata <quartenium@gmail.com>
2017-02-09 23:09:02 +0300

    dlopen for dinamyc loading SO lib and cast and call functions

--- share_lib/hello.c
+++ share_lib/hello.c
@@ -6,3 +6,7 @@ void hello_message(const char *name) {
 }
 // compile to Shared lib: gcc -o libName.so -shared -fPIC file.c
 // nm - !!! view names. nm libSome.so
+int runtime_fun(int a) {
+    printf("from dinamyc loaded\n");
+    return a*a*a;
+}
--- share_lib/main.c
+++ share_lib/main.c
@@ -1,15 +1,28 @@
-#include "hello.h"
+//#include "hello.h"
 #include <stdio.h>
 #include <stddef.h>
-#include "solution.h"
+//#include "solution.h"
+#include <dlfcn.h>
+
+void (*hello_message)(const char *);
+int (*runtime_fun)(int);
 
 int main() {
-    hello_message("John");
+    //hello_message("John");
     char *s = "Str";
-    int a = 8;
-    int b;
-    b = stringStat(s, 2, &a);
-    printf("\n b = %i a = %i\n", b, a);
+    void *lib = dlopen("libHello.so", RTLD_LAZY);
+    if (NULL == lib)
+        printf("lib not loaded");
+    hello_message = (void (*) (const char *))dlsym(lib, "hello_message");
+    runtime_fun = (int (*) (int))dlsym(lib, "runtime_fun");
+    if (NULL == runtime_fun || NULL == hello_message)
+        printf("funcs not founded");
+    hello_message("YEar!");
+    hello_message(s);
+    runtime_fun(123);
+    // cast
+    // void runtime_fun(int a);
+    // run
     return 0;
 }
 //gcc main.c -L. -lHello -o hello
