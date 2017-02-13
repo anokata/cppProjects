079ce581e5325728803be5cc499181a893c2a812
anokata <quartenium@gmail.com>
2017-02-13 11:31:30 +0300

    time test

--- exercises/test.c
+++ exercises/test.c
@@ -1,9 +1,24 @@
-#include <inttypes.h>
+#include <inttypes.h> // wcstoimax
 #include <stdint.h>
 #include <stdio.h>
 #include <unistd.h>
+//#define NDEBUG
+#include <assert.h>
+//#include <ctype.h> // tolower ... // wctype.h
+#include <stddef.h>
+#include <time.h>
 
 int main() {
+    const time_t timer = time(NULL);
+    printf("%s\n", ctime(&timer));
+    struct tm * timeinfo;
+    timeinfo = localtime(&timer);
+    char * t = asctime(timeinfo);
+    printf("%s\n", t);
+    char buffer [80];
+    strftime (buffer,80,"Now it's %y/%m/%d.",timeinfo);
+    puts (buffer);
+
     void *a;
     printf("pointer size: %lu bytes\n", sizeof(a));
     long l1dcls = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
@@ -11,6 +26,7 @@ int main() {
     // dynamic array
     uint32_t n = 0;
     scanf("%" SCNu32, &n);
+    assert(n > 2 && "MUST BE > 2");
     uint32_t array[n];
     for (uint32_t i = 0; i < n; i++) {
         array[i] = i * i * 3;
