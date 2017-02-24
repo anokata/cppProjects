846a640baeb9f52becaf5ecb1a6363a02616b63d
anokata <quartenium@gmail.com>
2017-02-24 11:01:22 +0300

    more error test. environment vars view

--- exercises/c/err.c
+++ exercises/c/err.c
@@ -1,5 +1,8 @@
-#include <string.h>
-#include <unistd.h>
+#include <string.h> // strlen?
+#include <unistd.h> // write
+#include <stdio.h> // fprintf
+#include <errno.h> // errno
+extern char **environ;
 
 void error_test() {
     for (int i = 0; i < 10; i++) {
@@ -9,8 +12,26 @@ void error_test() {
         write(1, "\n", 2);
     }
     perror("Error> ");
+    //fprintf(stdin, "mem>> %s\n", strerror(ENOMEM));
+    errno = ENOMEM;
+    perror("self: ");
+    for (int i = 0; i < 10; i++) {
+        errno = i*i;
+        char* str = strerror(i);
+        int len = strlen(str);
+        perror("this: ");
+    }
 }
 
-int main() {
+void env_test() {
+    for (int i = 0; i < 3; i++) {
+        if (environ[i] != NULL)
+            printf("env[%d] : %s\n", i, environ[i]);
+    }
+}
+
+int main(int argc, char* argv[], char* envp[]) {
+    error_test();
+    env_test();
     return 0;
 }
