0b254ad8b1f1b34dc1b4b2eba7712ed9943012e2
anokata <quartenium@gmail.com>
2017-03-01 17:09:16 +0300

    use glib. refactor. use aafont

--- exercises/c/sdltest.c
+++ exercises/c/sdltest.c
@@ -1,60 +1,54 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_ttf.h>
 #include <SDL/SDL_gfxPrimitives.h>
+#include <SDL/SDL_framerate.h>
+#include <glib.h>
 
-//TODO: basic lines
-#define FRAMES_PER_SECOND 20
+//TODO: 
 
-static uint32_t start = 0;
-void start_timer() {
-    start = SDL_GetTicks();
-}
-uint32_t diff_timer() {
-    uint32_t old_start = start;
-    start = SDL_GetTicks();
-    return start - old_start;
-}
-uint32_t get_timer() {
-    uint32_t end = SDL_GetTicks();
-    return end - start;
-}
+#define FRAMES_PER_SECOND 60
 
-int main( int argc, char* args[] )
-{
+static FPSmanager frames;
+static SDL_Surface* screen = NULL;
+static TTF_Font* font = NULL;
+static SDL_Surface* hello = NULL;
+
+int 
+sdl_init() {
     SDL_Init( SDL_INIT_EVERYTHING );
-    SDL_Surface* hello = NULL;
-    SDL_Surface* screen = NULL;
-    SDL_Event event;
-    int quit = 0;
-    TTF_Font* font = NULL;
-    SDL_Color textColor = {255, 0, 255};
-    int frame = 0;
-
-    screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
     TTF_Init();
+    screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
+    SDL_initFramerate(&frames);
+    SDL_setFramerate(&frames, FRAMES_PER_SECOND);
     font = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 20);
     if (!font) {
         perror("openfont");
-        exit(1);
+        return -1;
     }
-    hello = TTF_RenderText_Solid(font, "Hello text", textColor);
+    return 0;
+}
+
+int 
+init_render() {
+    SDL_Color textColor = {255, 0, 255};
+    SDL_Color bgColor = {0, 0, 0};
+    hello = TTF_RenderText_Shaded(font, "Hello text", textColor, bgColor);
     if (hello == NULL) {
         perror("render text");
-        return 1;
+        return -1;
     }
-    SDL_BlitSurface( hello, NULL, screen, NULL );
+    SDL_Rect r = {10, 200};
+    SDL_BlitSurface( hello, NULL, screen, &r );
     SDL_Flip( screen );
-    uint32_t* pixels = screen->pixels;
-    for (uint32_t x = 0; x < 100; x++) {
-        pixels[x*x] = 0xFF0000;
-    }
-    //lineRGBA(screen, 0, 0, 100, 50, 100, 0, 0, 255);
-    aalineRGBA(screen, 0, 0, 100, 50, 100, 0, 0, 200);
     boxColor(screen, 10, 10, 100, 50, 0xFFFF00AA);
-    //filledEllipseRGBA(screen, 100, 100, 100, 50, 200, 0, 100, 255);
-    aaellipseRGBA(screen, 100, 100, 100, 50, 200, 0, 100, 255);
+    return 0;
+}
+
+int 
+main_loop() {
+    SDL_Event event;
+    int quit = 0;
     while (quit == 0) {
-        start_timer();
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) quit = 1;
             if (event.type == SDL_KEYDOWN) {
@@ -68,14 +62,39 @@ int main( int argc, char* args[] )
             }
         }
         SDL_Flip( screen );
-        frame++;
-        if (get_timer() < 1000/FRAMES_PER_SECOND) {
-            SDL_Delay( (1000 / FRAMES_PER_SECOND) - get_timer() );
-            diff_timer();
-            //printf("frame delay %d\n", frame);
-        }
+        SDL_framerateDelay(&frames);
     }
+    return 0;
+}
+
+void 
+sdl_free() {
     SDL_FreeSurface( hello );
     SDL_Quit();
+}
+
+void glibtest() {
+    GList *l = NULL;
+    int z = 0x00414200;
+    l = g_list_append(l, "first");
+    l = g_list_append(l, &z);
+    l = g_list_append(l, "t");
+    GList *p = l;
+    while (p != NULL) {
+        char* str = p->data;
+        printf("%s\n", str);
+        p = p->next;
+    }
+    g_list_free(l);
+}
+
+int 
+main( int argc, char* args[] )
+{
+    glibtest();
+    sdl_init();
+    init_render();
+    main_loop();
+    sdl_free();
     return 0;
 }
