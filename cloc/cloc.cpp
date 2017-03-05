dc1380441de34b09e8c4f5f13ca84791f2383376
anokata <quartenium@gmail.com>
2017-03-05 17:24:08 +0300

    start make cloc. knew str iter need learn

---
+++ cloc/cloc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+bool is_count_line(std::string s) {
+    if (s == "") return false;
+}
+
+int main(int argc, char** argv) {
+    std::string in;
+    std::cin >> in;
+
+    return 0;
+}
