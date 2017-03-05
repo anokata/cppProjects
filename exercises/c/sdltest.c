7f09dfa1b1c3641a704fc5a682c71bc67c01e405
anokata <quartenium@gmail.com>
2017-03-05 12:34:34 +0300

    chg demo. make render double buf

--- exercises/c/sdltest.c
+++ exercises/c/sdltest.c
@@ -4,6 +4,8 @@
 #include <SDL/SDL_framerate.h>
 #include <glib.h>
 
+uint32_t rmask, gmask, bmask, amask;
+
 //TODO: make demo scene
 
 #define FRAMES_PER_SECOND 60
@@ -28,7 +30,7 @@ typedef struct {
 void render_list(SDL_Surface* surface, GList* list, uint32_t x, uint32_t y);
 void add_node(char* text);
 void demo_run();
-void demo(int tick);
+void demo(int tick, SDL_Surface*);
 
 static FPSmanager frames;
 static SDL_Surface* screen = NULL;
@@ -36,12 +38,25 @@ static TTF_Font* font = NULL;
 
 static GList* test_list = NULL;
 static node_options opts = {{0x60, 0x80, 0xC0}, {0x30, 0, 0x40}, 30, 10};
+static const int width = 640;
+static const int height = 480;
 
 int 
 sdl_init() {
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+    rmask = 0xff000000;
+    gmask = 0x00ff0000;
+    bmask = 0x0000ff00;
+    amask = 0x000000ff;
+#else
+    rmask = 0x000000ff;
+    gmask = 0x0000ff00;
+    bmask = 0x00ff0000;
+    amask = 0xff000000;
+#endif
     SDL_Init( SDL_INIT_EVERYTHING );
     TTF_Init();
-    screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
+    screen = SDL_SetVideoMode( width, height, 32, SDL_SWSURFACE );
     SDL_initFramerate(&frames);
     SDL_setFramerate(&frames, FRAMES_PER_SECOND);
     font = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 20);
@@ -86,6 +101,18 @@ void key_handler(char k) {
 	}
 }
 
+void render() {
+	SDL_Surface* surf = SDL_CreateRGBSurface(SDL_HWSURFACE, width, height, 32, rmask, gmask, bmask, amask);
+	boxColor(surf, 0, 0, width, height, 0x0FF);
+
+    render_list(surf, test_list, 100, 50);
+    demo_run(surf);
+
+	SDL_BlitSurface(surf, NULL, screen, NULL);
+    SDL_Flip(screen);
+    SDL_FreeSurface(surf);
+}
+
 int 
 main_loop() {
     SDL_Event event;
@@ -108,9 +135,7 @@ main_loop() {
 				}
             }
         }
-        render_list(screen, test_list, 100, 50);
-		demo_run();
-        SDL_Flip(screen);
+        render();
         SDL_framerateDelay(&frames);
     }
     return 0;
@@ -146,11 +171,11 @@ void render_list(SDL_Surface* surface, GList* list, uint32_t x, uint32_t y) {
         img = TTF_RenderText_Shaded(font, text, options->text_color, options->rect_color);
 		width = img->w + options->padx; 
 		height = img->h + options->pady;
-        Uint32 rgbColor = SDL_MapRGB(screen->format, 
+        Uint32 rgbColor = SDL_MapRGB(surface->format, 
 				options->rect_color.r, 
 				options->rect_color.g, 
 				options->rect_color.b);
-        boxRGBA(screen, r.x, r.y, r.x + width, r.y+height, 
+        boxRGBA(surface, r.x, r.y, r.x + width, r.y+height, 
                 options->rect_color.r,
                 options->rect_color.g,
                 options->rect_color.b,
@@ -158,25 +183,33 @@ void render_list(SDL_Surface* surface, GList* list, uint32_t x, uint32_t y) {
                 );
 		r.x += (width - img->w) / 2;
 		r.y += (height - img->h) / 2;
-        SDL_BlitSurface(img, NULL, screen, &r);
+        SDL_BlitSurface(img, NULL, surface, &r);
         cur = cur->next;
         r.y += height;
     }
 }
 
-void demo_run() {
+void demo_run(SDL_Surface* screen) {
 	static int t = 0;
-	const int m = 10;
-	if (t % m == 0) {
-		demo(t++ / m);
-	} else t++;
+	const int m = 5;
+	//if (t % m == 0) {
+		demo(t++ / m, screen);
+	//} else t++;
 }
-void demo(int tick) {
+void demo(int tick, SDL_Surface* screen) {
+    if (tick < 100) {
     SDL_Surface* img = NULL;
 	char text[20];
 	sprintf(text, "%d", tick);
 	img = TTF_RenderText_Shaded(font, text, opts.text_color, opts.rect_color);
 	SDL_BlitSurface(img, NULL, screen, NULL);
+    SDL_Surface* surf;
+	surf = SDL_CreateRGBSurface(SDL_HWSURFACE, 100, 100, 32, rmask, gmask, bmask, amask);
+	rectangleColor(surf, 50, 50, 20, 20, 0xFF00FFFF);
+	rectangleColor(surf, 49, 49, 21, 21, 0xFF00FFFF);
+	rectangleColor(surf, 49-tick, 49-tick, 21+tick, 21+tick, 0xFF00FFFF*tick);
+	SDL_BlitSurface(surf, NULL, screen, NULL);
+    }
 }
 
 void glibtest() {
