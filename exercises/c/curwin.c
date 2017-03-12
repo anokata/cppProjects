e81d8363acb4be64e62872f3970852f75bed13f1
anokata <quartenium@gmail.com>
2017-03-12 23:24:25 +0300

    curses window refactor some

--- exercises/c/curwin.c
+++ exercises/c/curwin.c
@@ -1,6 +1,12 @@
 #include <ncurses.h>
 #include <stdio.h>
-int main() {
+
+#define MAX_WINDOWS 4
+static int width;
+static int heigth;
+static WINDOW* wins[MAX_WINDOWS];
+
+void init() {
 	initscr();
 	start_color();
 	init_pair(1, COLOR_RED, COLOR_BLACK);
@@ -10,25 +16,19 @@ int main() {
 	init_pair(5, COLOR_WHITE, COLOR_BLACK);
 	attron(COLOR_PAIR(4));
 	refresh();
-    int width = getmaxx(stdscr);
-    int heigth = getmaxy(stdscr);
+    width = getmaxx(stdscr);
+    heigth = getmaxy(stdscr);
+}
 
-	WINDOW* win1 = newwin(heigth/1, width/2, 0, 0);
-	wattron(win1, 0);
-	wbkgdset(win1, COLOR_PAIR(4)  | ' ');
-	wclear(win1);
-    for (int i=0; i< heigth*width/1; i++) {
-        //wprintw(win1, ". ");
-        wclrtoeol(win1);
-    }
-    box(win1, 0, 0);
-    wmove(win1, 1, 1);
-	wprintw(win1, "somenewwindowtex");
-    refresh();
-	wrefresh(win1);
+void end() {
+	endwin();
+	printf("%d:%d", width, heigth/2 - 1);
+}
 
+// setKeyHandler(void (*func)())
+//chtype a = 'a' | A_BOLD | COLOR_PAIR(1);
+void processInput() {
 	int ch = getch();
-	//chtype a = 'a' | A_BOLD | COLOR_PAIR(1);
 	while(ch != 'q') {
 		clear();
 		if (ch != '\t');
@@ -37,8 +37,34 @@ int main() {
 		refresh();
 		ch = getch();
 	}
+}
 
-	endwin();
-	printf("%d:%d", width, heigth/2 - 1);
+WINDOW* make_window() {
+	WINDOW* win = newwin(heigth/1, width/2, 0, 0);
+	wattron(win, 0);
+	wbkgdset(win, COLOR_PAIR(4)  | ' ');
+	wclear(win);
+    return win;
+}
+void win1_refresh() {
+    WINDOW* win1 = wins[0];
+    box(win1, 0, 0);
+    wmove(win1, 1, 1);
+	wprintw(win1, "somenewwindowtex");
+    refresh();
+	wrefresh(win1);
+}
+
+void makeWins() {
+	WINDOW* win1 = (make_window());
+    wins[0] = win1;
+    win1_refresh();
+}
+
+int main() {
+    init();
+    makeWins();
+    processInput();
+    end();
 	return 0;
 }
