b5a5763615deb8965da41c77c6f9a867bce1721e
anokata <quartenium@gmail.com>
2017-03-12 15:24:15 +0300

    interpolation Lagrange method

--- exercises/number_methods.cpp
+++ exercises/number_methods.cpp
@@ -42,9 +42,37 @@ double interpolate_line(double x, tab_data *d) {
     d->second.insert(d->second.begin() + index, y);
 }
 
+double interpolate_with(double x, tab_data *d, double (*func)(double, tab_data)) {
+    auto it = find_if(d->first.begin(), d->first.end(), [x](double s){return s > x;});
+    size_t index = distance(d->first.begin(), it);
+    point left = point{d->first.at(index), d->second.at(index)};
+    point right = point{d->first.at(index+1), d->second.at(index+1)};
+    double y = func(x, *d);
+    d->first.insert(d->first.begin() + index, x);
+    d->second.insert(d->second.begin() + index, y);
+}
+double Lj(vector<double> xs, double x, size_t j) {
+    double prod = 1;
+    for (size_t i = 0; i < xs.size(); ++i) {
+        if (i == j) continue;
+        prod *= (x - xs[i]);
+    }
+    return prod;
+}
+
+double interpolate_Lagrange(double x, tab_data d) {
+    double y = 0;
+    for (size_t j = 0; j < d.first.size(); ++j) {
+        double c = Lj(d.first, x, j) / Lj(d.first, d.first[j], j);
+        y += d.second[j] * c;
+    }
+    return y;
+}
+
 void tabulate_func() {
     auto p = sampler(-2, 2, 0.15, &sample1);
-    double y = interpolate_line(0.123, &p);
+    double ly = interpolate_with(0.123, &p, &interpolate_Lagrange);
+    //double y = interpolate_line(0.123, &p);
     auto xs = p.first;
     auto ys = p.second;
     for (int i = 0; i < xs.size(); ++i) {
