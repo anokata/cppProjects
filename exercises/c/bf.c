2ca44a527646f08946a1e27f2ac9db9d750b8402
anokata <quartenium@gmail.com>
2017-03-14 18:11:48 +0300

    Make loops and run. Fix input

--- exercises/c/bf.c
+++ exercises/c/bf.c
@@ -1,37 +1,61 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 //optarg
 
 #define BF_MEMORY_SIZE 30000
+#define BF_LOOPS_SIZE 1000
 typedef struct Machine {
     uint8_t *data; 
     uint8_t dp; // Data pointer
     char *code; 
     uint8_t ip; // Instruction pointer 
-    // loops
     uint8_t ii; // Output index
-    uint8_t *input;
+    char *input;
+    uint8_t input_size;
     uint8_t oi; // Output index
-    uint8_t *output;
+    char *output;
+    uint32_t *loops;
+    uint32_t code_size;
 } Machine;
 
-Machine* make_machine(char* code, uint8_t* input) {
+Machine* make_machine(char* code, char* input) {
     Machine* m = malloc(sizeof(Machine));
     m->data = malloc(BF_MEMORY_SIZE);
+    m->loops = calloc(1, BF_LOOPS_SIZE);
+    memset(m->loops, 0, BF_LOOPS_SIZE);
     m->dp = 0;
     m->ip = 0;
     m->oi = 0;
     m->ii = 0;
     m->code = code;
+    m->code_size = strlen(code);
     m->output = malloc(100);
     m->input = input;
+    m->input_size = strlen(input);
+
+    uint32_t *loop_stack = malloc(BF_LOOPS_SIZE);
+    int l = 0;
+    for (int i = 0; i < strlen(code); i++) {
+        if (code[i] == '[') {
+            loop_stack[l++] = i;
+        }
+        if (code[i] == ']') {
+            l--;
+            m->loops[loop_stack[l]] = i;
+            m->loops[i] = loop_stack[l];
+        }
+    }
+    free(loop_stack);
 
     return m;
 }
 
 void down_machine(Machine* m) {
     free(m->output);
+    free(m->loops);
     free(m->data);
     free(m);
 }
@@ -57,6 +81,13 @@ void print_machine(Machine* m) {
         }
         printf("\n");
     }
+    printf("Loops: [%p]:  ", m->loops);
+    for (int i = 0; i < BF_LOOPS_SIZE/sizeof(uint32_t); i++) {
+        if (m->loops[i] && i < m->loops[i]) {
+            printf("%d->%d  ", i, m->loops[i]);
+        }
+    }
+    printf("\n");
 }
 
 void machine_cmd_dpinc(Machine* m) {
@@ -80,14 +111,56 @@ void machine_cmd_out(Machine* m) {
 }
 
 void machine_cmd_in(Machine* m) {
-    m->data[m->dp] = m->input[m->ii++];
+    if (m->ii < m->input_size) {
+        m->data[m->dp] = m->input[m->ii++];
+    } else {
+        m->data[m->dp] = 0;
+    }
 }
 
-void test_m() {
-    uint8_t input[] = "some";
-    char code[] = ",+[-.,+]";
-    Machine* m = make_machine(code, input);
-    printf("> Machine maked.\n");
+void machine_cmd_loop_start(Machine* m) {
+    if (m->data[m->dp] == 0) {
+        m->ip = m->loops[m->ip];
+    }
+}
+
+void machine_cmd_loop_end(Machine* m) {
+    if (m->data[m->dp] != 0) {
+        m->ip = m->loops[m->ip];
+    }
+}
+
+void machine_step(Machine* m) {
+    switch (m->code[m->ip]) {
+        case '>':
+            machine_cmd_dpinc(m);
+            break;
+        case '<':
+            machine_cmd_dpdec(m);
+            break;
+        case '+':
+            machine_cmd_datainc(m);
+            break;
+        case '-':
+            machine_cmd_datadec(m);
+            break;
+        case '.':
+            machine_cmd_out(m);
+            break;
+        case ',':
+            machine_cmd_in(m);
+            break;
+        case '[':
+            machine_cmd_loop_start(m);
+            break;
+        case ']':
+            machine_cmd_loop_end(m);
+            break;
+    }
+    m->ip++;
+}
+
+void test_instructions(Machine* m) {
     machine_cmd_dpinc(m);
     machine_cmd_datainc(m);
     machine_cmd_dpinc(m);
@@ -102,6 +175,25 @@ void test_m() {
     machine_cmd_dpinc(m);
     machine_cmd_in(m);
     machine_cmd_out(m);
+}
+
+void machine_run(Machine* m) {
+    while (m->ip < m->code_size) {
+        machine_step(m);
+        print_machine(m);
+        usleep(100000);
+    }
+}
+
+void test_m() {
+    char input[] = "some\xFF";
+    char code[] = ",+[-.,+]";
+    Machine* m = make_machine(code, input);
+    printf("> Machine maked.\n");
+    machine_run(m);
+
+    //test_instructions(m);
+    
     print_machine(m);
     down_machine(m);
 }
