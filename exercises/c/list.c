d70dbf87f095520f44491bbff6a3bd82562fceac
anokata <quartenium@gmail.com>
2017-03-16 18:37:35 +0300

    list implimenting...

--- exercises/c/list.c
+++ exercises/c/list.c
@@ -8,11 +8,31 @@ typedef struct List {
 
 typedef void* (*List_map_func)(void*);
 
-List* list_add(List** list, void* data) {
-    List* new_elem;
+List* list_add(List** list, void* data);
+
+List* list_new(void* data) {
+    List* new_elem; 
     new_elem = malloc(sizeof(*new_elem));
     new_elem->data = data;
     new_elem->next = 0;
+    return new_elem;
+}
+
+void list_push(List** list, void* data) {
+    List* new_elem = list_new(data);
+
+    if (*list == 0) {
+        (*list) = new_elem;
+        return;
+    }
+    while ((*list)->next) {
+        *list = (*list)->next;
+    }
+    (*list)->next = new_elem;
+}
+
+List* list_add(List** list, void* data) {
+    List* new_elem = list_new(data);
 
     if (list == 0) {
         return new_elem;
@@ -52,21 +72,35 @@ void list_p(List* list) {
     list_map(list, list_print_func);
 }
 
-// safe(func to save one elem(fd))
-// map list
+
+// save(func to save one elem(fd))
+// push pop append remove take head tail slice reduce
+// delete delete_map
+// save/load serialization
 #define DEBUG
 
 #ifdef DEBUG
+#include <string.h>
 int main() {
     List *l; 
     int a = 812;
-    char s[] = "a;d;d'z'";
+    char s[] = "Linked List.";
     l = list_add(0, &a);
     l = list_add(&l, &s);
     printf("%d\n", *(int*)l->next->data);
     list_add(&l, &s);
+    for (int i = 0; i < strlen(s); i++) {
+        //list_add(&l, s+i);
+    }
     printf("%s\n", (char*)l->data);
     list_print(l);
+    list_p(l);
+
+    int b = 3; int c = 4;
+    l = 0;
+    list_push(l, &a);
+    list_push(l, &b);
+    list_push(l, &c);
     list_p(l);
     return 0;
 }
