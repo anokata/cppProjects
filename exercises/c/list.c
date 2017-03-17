ac992a05999f14c99758c2bff3c27f95de34842b
anokata <quartenium@gmail.com>
2017-03-17 18:01:08 +0300

    Save and load implemented into binary and tag

--- exercises/c/list.c
+++ exercises/c/list.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 
 typedef void (*free_node_fnc)(void*);
+typedef char* (*data_to_str_fnc)(void*);
+
 typedef struct ListNode { // Node
     struct ListNode* next;
     void* data;
@@ -12,7 +15,9 @@ typedef struct List {
     ListNode* head;
     ListNode* tail;
     uint32_t length;
+    uint32_t data_size;
     free_node_fnc free_fnc;
+    data_to_str_fnc tostr_fnc;
 } List;
 
 typedef void* (*List_map_func)(void*);
@@ -32,12 +37,20 @@ void free_node_stumb(void* ptr) {
     printf("* Free at %p\n", ptr);
 }
 
+char* data_to_str_stumb(void* data) { // ALLOC
+    char* str = malloc(sizeof(uint32_t) + 1);
+    sprintf(str, "%d", *((uint32_t*)data));
+    return str;
+}
+
 List* list_new() {
     List* list = malloc(sizeof(*list));
     list->length = 0;
     list->head = 0;
     list->tail = 0;
     list->free_fnc = free_node_stumb;
+    list->tostr_fnc = data_to_str_stumb;
+    list->data_size = sizeof(uint32_t);
     return list;
 }
 
@@ -173,6 +186,66 @@ void list_delete(List* list) {
     list->tail = 0;
 }
 
+const static uint32_t LIST_MAGIC = 
+    0b01110000 | 
+    0b00100000 << 8| 
+    0b00100100 << 16| 
+    0b00111100 << 24;
+const static uint32_t LIST_TAG = 'L' + ('I' << 8) + ('S' << 16) + ('T' << 24);
+
+int list_save2file(List* list, char* fn) {
+	FILE *fd = fopen(fn, "w");
+	if (!fd) {
+		perror("open");
+		return -1;
+	}
+    // TODO first serialize to memory, after write once
+	size_t bytes = fwrite(&LIST_TAG, sizeof(LIST_TAG), 1, fd);
+    ListNode* cur = list->head;
+    bytes += fwrite(&list->length, sizeof(uint32_t), 1, fd);
+    bytes += fwrite(&list->data_size, sizeof(uint32_t), 1, fd);
+    while (cur) {
+        // TODO convert data to serialized
+        bytes += fwrite(cur->data, list->data_size, 1, fd);
+        cur = cur->next;
+    }
+	printf("* writed %ld items\n", bytes);
+	fclose(fd);
+	return 0;
+}
+
+List *list_loadfile(char* fn) {
+    FILE *fd = fopen(fn, "r");
+    if (!fd) {
+        perror("open for load");
+        return 0;
+    }
+    List *list = list_new();
+    // check tag
+    uint32_t tag;
+    size_t readed = fread(&tag, sizeof(tag), 1, fd);
+    if (!readed) {
+        perror("not read");
+        return 0;
+    }
+    if (tag != LIST_TAG) {
+        perror("Tag not is LIST");
+        return 0;
+    }
+    uint32_t length;
+    uint32_t size;
+    void *data;
+    fread(&length, sizeof(list->length), 1, fd);
+    fread(&size, sizeof(list->data_size), 1, fd);
+    list->data_size = size;
+    for (size_t i = 0; i < length; i++) {
+        data = malloc(size);
+        fread(data, size, 1, fd);
+        list_push(list, data);
+    }
+
+    return list;
+}
 
 // save(func to save one elem(fd))
 // push pop append remove take head tail slice reduce
@@ -184,6 +257,7 @@ void list_delete(List* list) {
 #ifdef DEBUG
 #include <string.h>
 void test() {
+    //separate test one func
     List *l; 
     int a = 812;
     char s[] = "Linked List.";
@@ -196,6 +270,7 @@ void test() {
     list_print(l);
     list_p(l);
     int b = 3; int c = 4;
+    list_delete(l);
     l = list_new();
     list_push(l, &a);
     list_push(l, &b);
@@ -205,6 +280,7 @@ void test() {
     int x2 = 2;
     int x3 = 3;
     int d = 5;
+    list_delete(l);
     l = list_new();
     list_push(l, &x1);
     list_push(l, &x2);
@@ -223,6 +299,7 @@ void test() {
     printf("t = %p\n", (int*)l->tail);
     printf("\n");
     list_p(l);
+    list_delete(l);
     l = list_new();
     list_push(l, &x1);
     list_push(l, &x2);
@@ -236,8 +313,27 @@ void test() {
     list_delete(l);
     list_p(l);
 }
+
+void test_save() {
+    int a = 1; int b = 2;
+    List* l = list_new();
+    list_push(l, &a);
+    list_add(l, &b);
+    list_save2file(l, "testsavelist");
+    list_remove(l);
+}
+
+void test_load() {
+    List *l = list_loadfile("testsavelist");
+    printf("loaded %d items\n", l->length);
+    list_p(l);
+    list_remove(l);
+}
+
 int main() {
-    test();
+    //test();
+    //test_save();
+    test_load();
     return 0;
 }
 #endif
