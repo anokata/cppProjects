54e6c952e782ef9ecea08f30375a67349d83684e
anokata <quartenium@gmail.com>
2017-03-17 14:33:50 +0300

    Refactor list struct.

--- exercises/c/list.c
+++ exercises/c/list.c
@@ -1,69 +1,103 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 
-typedef struct List { // Node
-    struct List* next;
+typedef struct ListNode { // Node
+    struct ListNode* next;
     void* data;
-    //on delete func. store on main list struct.
+} ListNode;
+
+//on delete func. store on main list struct.
+typedef struct List {
+    ListNode* head;
+    ListNode* tail;
+    uint32_t length;
 } List;
 
 typedef void* (*List_map_func)(void*);
+ListNode* list_newnode(void* data);
+List* list_new();
+int list_push(List* list, void* data);
+int list_add(List* list, void* data);
+void* list_pop(List* list);
 
-List* list_add(List** list, void* data);
+List* list_new() {
+    List* list = malloc(sizeof(*list));
+    list->length = 0;
+    list->head = 0;
+    list->tail = 0;
+    return list;
+}
 
-List* list_new(void* data) {
-    List* new_elem; 
+ListNode* list_newnode(void* data) {
+    ListNode* new_elem; 
     new_elem = malloc(sizeof(*new_elem));
     new_elem->data = data;
     new_elem->next = 0;
     return new_elem;
 }
 
-void list_push(List* list, void* data) {
-    if (list == 0) {
-        return;
+int list_push(List* list, void* data) {
+    ListNode* ln = list->head;
+    ListNode* new_elem = list_newnode(data);
+    list->length++;
+    if (ln == 0) {
+        list->head = new_elem;
+        return 0;
     }
-    List* new_elem = list_new(data);
-    while (list->next) {
-        list = list->next;
+    while (ln->next) {
+        ln = ln->next;
     }
-    list->next = new_elem;
+    ln->next = new_elem;
+    list->tail = new_elem;
+    return 0;
 }
 
-void* list_pop(List** list) {
-    List* lst = *list;
-    if (lst->next == 0) {
+void* list_pop(List* list) {
+    ListNode* lst = list->head;
+    if (list->length == 0) {
+        return 0;
+    }
+    if (list->length == 1) {
         //free
         void* data = lst->data;
-        list = 0;
+        list->head = 0;
+        list->tail = 0;
+        list->length--;
         return data;
     } else {
+        list->length--;
         while (lst->next->next != 0) {
             lst = lst->next;
         }
         void* data = lst->next->data;
         //free
         lst->next = 0;
+        list->tail = lst;
         return data;
     }
 }
 
-List* list_add(List** list, void* data) { // no empty
-    List* new_elem = list_new(data);
+int list_add(List* list, void* data) {
+    ListNode* new_elem = list_newnode(data);
+    list->length++;
 
-    if (list == 0) {
-        return new_elem;
+    if (list->head == 0) {
+        list->head = new_elem;
+        list->tail = new_elem;
+        return 0;
     } else {
-        new_elem->next = *list;
-        *list = new_elem;
-        return new_elem;
+        new_elem->next = list->head;
+        list->head = new_elem;
+        return 0;
     }
 }
 
 void list_map(List* list, List_map_func f) {
-    while (list) {
-        list->data = f(list->data);
-        list = list->next;
+    ListNode *ln = list->head;
+    while (ln) {
+        ln->data = f(ln->data);
+        ln = ln->next;
     }
 }
 
@@ -75,12 +109,13 @@ void* list_print_func(void* data) {
 
 void list_print(List *list) {
     int i = 1;
-    while (list) {
+    ListNode *ln = list->head;
+    while (ln) {
         printf("Item[%d] ", i++);
-        printf("next: %p  ", list->next);
-        printf("data: %p int(%d) long(%ld) ", list->data, *(int*)list->data, *(long*)list->data);
-        printf("str(%s) \n", (char*)list->data);
-        list = list->next;
+        printf("next: %p  ", ln->next);
+        printf("data: %p int(%d) long(%ld) ", ln->data, *(int*)ln->data, *(long*)ln->data);
+        printf("str(%s) \n", (char*)ln->data);
+        ln = ln->next;
     }
     printf("\n");
 }
@@ -102,22 +137,42 @@ int main() {
     List *l; 
     int a = 812;
     char s[] = "Linked List.";
-    l = list_add(0, &a);
-    l = list_add(&l, &s);
-    printf("%d\n", *(int*)l->next->data);
-    list_add(&l, &s);
-    for (int i = 0; i < strlen(s); i++) {
-        //list_add(&l, s+i);
-    }
-    printf("%s\n", (char*)l->data);
+    l = list_new();
+    list_add(l, &a);
+    list_add(l, &s);
+    printf("%d\n", *(int*)l->head->next->data);
+    list_add(l, &s);
+    printf("%s\n", (char*)l->head->data);
     list_print(l);
     list_p(l);
 
     int b = 3; int c = 4;
-    l = list_new(&s);
+    l = list_new();
     list_push(l, &a);
     list_push(l, &b);
     list_push(l, &c);
+    list_p(l);
+    int x1 = 1;
+    int x2 = 2;
+    int x3 = 3;
+    int d = 5;
+    l = list_new();
+    list_push(l, &x1);
+    list_push(l, &x2);
+    list_push(l, &x3);
+    list_add(l, &c);
+    list_add(l, &d);
+    printf("pop = %d len(%d)\n", *(int*)list_pop(l), l->length);
+    printf("pop = %d len(%d)\n", *(int*)list_pop(l), l->length);
+    printf("pop = %d len(%d)\n", *(int*)list_pop(l), l->length);
+    printf("h = %d\n", *(int*)l->head->data);
+    printf("t = %d\n", *(int*)l->tail->data);
+    printf("xh = %p\n", (int*)l->head);
+    printf("xt = %p\n", (int*)l->tail);
+    printf("pop = %d len(%d)\n", *(int*)list_pop(l), l->length);
+    printf("h = %p\n", (int*)l->head);
+    printf("t = %p\n", (int*)l->tail);
+    printf("\n");
     list_p(l);
     return 0;
 }
