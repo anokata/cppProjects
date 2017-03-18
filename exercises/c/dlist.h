8a546804083dbea17234cf4ffd9ec73bd52e327f
anokata <quartenium@gmail.com>
2017-03-18 13:55:32 +0300

    Extract to headers dlist

---
+++ exercises/c/dlist.h
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+// Double linked list
+
+typedef void (*free_node_fnc)(void*);
+typedef void* (*List_map_func)(void*);
+typedef char* (*data_to_str_fnc)(void*);
+
+void free_node_stumb(void* ptr) {
+    printf("* Free at %p\n", ptr);
+    //free(ptr);
+}
+
+char* data_to_str_stumb(void* data) { // ALLOC
+    char* str = malloc(sizeof(uint32_t) + 1);
+    sprintf(str, "%d", *((uint32_t*)data));
+    return str;
+}
+
+typedef struct DListNode { 
+    struct DListNode* next;
+    struct DListNode* back;
+    void* data;
+} DListNode;
+
+typedef struct DList {
+    DListNode* head;
+    DListNode* tail;
+    uint32_t length;
+    uint32_t data_size;
+    free_node_fnc free_fnc;
+    data_to_str_fnc tostr_fnc;
+} DList;
+
+DListNode* list_newnode(void* data);
+DList* list_new();
+void list_p(DList* list);
+void list_print(DList *list);
+void list_map(DList* list, List_map_func f);
+void* list_print_func(void* data);
+void* list_pop(DList* list);
+int list_push(DList* list, void* data);
+int list_add(DList* list, void* data);
+int list_remove(DList* list);
+void list_delete(DList* list);
+
