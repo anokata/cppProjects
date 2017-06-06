96874d6bf97aa0f3db78cd600948e0b53dd61ee5
anokata <anokata8@mail.ru>
2017-06-06 08:44:52 +0300

    extract util functions

--- nsnake/app.cpp
+++ nsnake/app.cpp
@@ -5,25 +5,10 @@
 #include <sstream>
 #include <vector>
 #include <iterator>
+#include "util.h"
 //TODO  curses easy interface class: color manager? win, drawer
 //window class
 //
-
-template<typename Out>
-void split(const std::string &s, char delim, Out result) {
-    std::stringstream ss;
-    ss.str(s);
-    std::string item;
-    while (std::getline(ss, item, delim)) {
-        *(result++) = item;
-    }
-}
-
-std::vector<std::string> split(const std::string &s, char delim) {
-    std::vector<std::string> elems;
-    split(s, delim, std::back_inserter(elems));
-    return elems;
-}
 
 void print_by_line(std::string str, int x, int y) {
     auto lines = split(str, '\n');
