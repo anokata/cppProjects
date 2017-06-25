fe89a8b0af1b6878f4411304f9b387482169fcdd
anokata <anokata8@mail.ru>
2017-06-25 15:59:08 +0300

    [day] Started dayri project.

---
+++ day/csv.h
@@ -0,0 +1,6 @@
+#pragma once
+#ifndef __CSV__
+#define __CSV__
+// Read to vector of strings
+
+#endif
---
+++ day/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "csv.h"
+
+int main(int argc, char *argv[]) {
+    std::cout << "(l)";
+    return 0;
+}
