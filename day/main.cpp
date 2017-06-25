653419a863d6899d71445e6e20f9871cb6ac7a03
anokata <anokata8@mail.ru>
2017-06-25 17:03:11 +0300

    making csv prepare parse

--- day/main.cpp
+++ day/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "csv.h"
 
 int main(int argc, char *argv[]) {
-    std::cout << "(l)";
+    std::cout << "(l)\n";
+
+    std::string filename = "sample.csv";
+    char delimeter = ',';
+    std::ifstream in;
+    in.open(filename);
+    std::string buf;
+
+    /* Read headers */
+    std::getline(in, buf, '\n');
+    buf = "";
+
+    while (buf.find("\n") == std::string::npos) {
+        std::cout << buf;
+        std::getline(in, buf, delimeter);
+    }
+
     return 0;
 }
