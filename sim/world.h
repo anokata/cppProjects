ad1fe2724399a10a539261ca5597f9d4df7ef643
anokata <anokata8@mail.ru>
2017-07-18 18:05:11 +0300

    ...tz proc

--- sim/world.h
+++ sim/world.h
@@ -2,9 +2,22 @@
 #ifndef __WORLD__
 #define __WORLD__
 
+typedef float amount;
+
+class BaseCell {
+    amount oxygen;
+    amount hydrogen;
+    // not
+    // exist at gas, liquid, solid, temp? objects?
+    // atmosphere sim
+    // TODO Need clear requirements!
+};
+
 class EnergySpace {
     // Oxygen, Hydrogen, Carbon, Nitrogen...
     // algae make starch, use chlorophyll
+
+    // in cells of world exist oxygen etc materials in amount
 
     public:
         EnergySpace() {}
