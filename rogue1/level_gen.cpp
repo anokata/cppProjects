07bdbfd29501d9484d53fb272eb16e1c237e0b3a
anokata <anokata8@mail.ru>
2017-07-19 16:09:55 +0300

    (LevelGen) Making level generator, cave like

--- rogue1/level_gen.cpp
+++ rogue1/level_gen.cpp
@@ -1,39 +1,62 @@
 #include "level_gen.h"
 
+#include <iostream>
 /* Map chars definition */
 // ?
 
 char random_char() {
-    switch (std::rand() % 3) {
+    switch (std::rand() % 5) {
         case 0: return '.';
         case 1: return '#';
-        case 2: return ' ';
+        case 2: return '#';
+        case 3: return '#';
+        case 4: return ' ';
     }
     return ' ';
 }
 
 int neighbours(CharMap cm, Point p) {
-
-    return 0;
-}
-
-template <typename T> Map<T> step_generation(Map<T> cm) {
-    for (int i = 1; i < cm.width; i++) {
-        for (int k = 1; k < cm.height; k++) {
-            //cm.dots[Point(i, k)] = random_char();
+    int n = 0;
+    char current_char = cm[p];
+    for (int i = -1; i < 2; i++) {
+        for (int k = -1; k < 2; k++) {
+            if (cm[Point(i + p.first, k + p.second)] == current_char) {
+                n++;
+            }
         }
     }
+    return n;
+}
+
+template <typename T> Map<T> step_generation(Map<T> &cm) {
+    std::cout << cm.width;
+    for (int i = 1; i < cm.width; i++) {
+        for (int k = 1; k < cm.height; k++) {
+                /* cm.dots[Point(i, k)] = random_char(); */
+            if (neighbours(cm.dots, Point(i, k)) < 5) {
+                cm.dots[Point(i, k)] = ' ';
+            }
+            if (neighbours(cm.dots, Point(i, k)) > 7) {
+                cm.dots[Point(i, k)] = ' ';
+            }
+        }
+    }
+    return cm;
 }
 
 CharsMap generate() {
     CharMap cm;
-    for (int i = 1; i < 10; i++) {
-        for (int k = 1; k < 10; k++) {
+    int w = 20;
+    for (int i = 1; i < w; i++) {
+        for (int k = 1; k < w; k++) {
             cm[Point(i, k)] = random_char();
         }
     }
     CharsMap map;
     map.dots = cm;
+    map.width = w;
+    map.height = w;
+    step_generation(map);
 
     return map;
 }
