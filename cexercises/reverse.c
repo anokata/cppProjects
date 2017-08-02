7fd8cbb6fdc54131b3b8a50b4fec36b4d4c87405
anokata <anokata8@mail.ru>
2017-08-02 11:33:17 +0300

    extern test

--- cexercises/reverse.c
+++ cexercises/reverse.c
@@ -2,6 +2,8 @@
 
 void reverse(char *str, int len);
 
+extern void some(void);
+
 int main() {
     char *buf = NULL;
     size_t size = 0;
@@ -9,11 +11,12 @@ int main() {
     printf("%d\n%s", len, buf);
     reverse(buf, len);
     printf("%s", buf);
+    /* some(); */
 }
 
 void reverse(char *str, int len) {
     /* printf("rev in:%d\n%s", len, str); */
-    for (int i = 0; i < len / 2; ++i) { // SEE DIV TWO: must run to middle of string, else swap back to normal
+for (int i = 0; i < len / 2; ++i) { // SEE DIV TWO: must run to middle of string, else swap back to normal
         /* printf("step %d (%c)\n", i, str[i]); */
         char t = str[i];
         str[i] = str[len - i - 1];
